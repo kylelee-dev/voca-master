9a226683bd733aac3dc479bef91dc23d643ece68
Kyle Lee <kylelee@Kyles-MacBook-Air.local>
2023-08-23 18:01:14 -0400

    Apply subject context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthContextProvider } from "./context/authContext.tsx";
+import { SubjectContextProvider } from './context/subjectContext.tsx';
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SubjectContextProvider>
+        <App />
+      </SubjectContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
